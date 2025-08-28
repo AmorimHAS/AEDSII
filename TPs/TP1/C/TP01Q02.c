9a9ab7b7cce2ac2be3d6616be054aec2a20d8fdf
AmorimHAS <amorim.has@gmail.com>
2025-08-28 17:12:10 -0300

    T01Q02

--- TPs/TP1/C/TP01Q02.c
+++ TPs/TP1/C/TP01Q02.c
@@ -2,31 +2,41 @@
 #include <string.h>
 #include <stdbool.h>
 
-bool descobrirPalindromo(char str[],int TAM,int i){
-    if (i >= TAM){
+// função recursiva que verifica se uma string é palíndromo
+bool descobrirPalindromo(char str[], int TAM, int i) {
+    if (i >= TAM) { // caso base
         return true;
     }
-    if(str[i]!=str[TAM]){
+    if (str[i] != str[TAM]) { // se caracteres forem diferentes
         return false;
-    }else{
-        return descobrirPalindromo(str,TAM-1,i+1);
+    } else {
+        return descobrirPalindromo(str, TAM - 1, i + 1); // chama a função recursivamente, movendo índices
     }
 }
 
-int main(){
-    char str[10000];
-    while(fgets(str, sizeof(str), stdin) != NULL){
-        int TAM = strlen(str);
-        int i=0;
+int main() {
+    char str[10000]; // vetor de caracteres para receber a string
+
+    // lê linhas do input ate ser igual a FIM
+    while (fgets(str, sizeof(str), stdin) != NULL) {
+        int TAM = strlen(str); // obtém o tamanho da string
+        int i = 0; // índice inicial para verificação de palíndromo
+
+        // remove o '\n' no final da string, se existir
         if (TAM > 0 && str[TAM - 1] == '\n') {
             str[--TAM] = '\0';
         }
-        bool palindromo = descobrirPalindromo(str,TAM-1,i);
-        if(palindromo==1){
-        printf("SIM\n");
-        }else{
+
+        // chama a função recursiva para verificar se é palíndromo
+        bool palindromo = descobrirPalindromo(str, TAM - 1, i);
+
+        // imprime o resultado
+        if (palindromo == 1) {
+            printf("SIM\n"); 
+        } else {
             printf("NAO\n");
         }
     }
-    return 0;
+
+    return 0; // finaliza o programa
 }
