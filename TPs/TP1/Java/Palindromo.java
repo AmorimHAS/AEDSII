46ea980001cc175b55a698e359f18f2539d7fe1b
AmorimHAS <amorim.has@gmail.com>
2025-08-28 17:08:31 -0300

    TP1 pronto

--- TPs/TP1/Java/Palindromo.java
+++ TPs/TP1/Java/Palindromo.java
@@ -1,27 +1,35 @@
 import java.util.Scanner;
 
 public class Palindromo {
-    public static void main (String[] args){
+    public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
-        String palavra = sc.nextLine();
-        while(!(palavra.equals("FIM"))){
-            int tam = palavra.length() - 1;
-            int i=0;
-            boolean teste = true;
-            while(tam>i && teste == true){
-                if(palavra.charAt(tam) != palavra.charAt(i)){
-                    teste=false;
+        String palavra = sc.nextLine(); // lê a primeira linha de entrada
+
+        // enquanto a palavra digitada não for "FIM"
+        while(!(palavra.equals("FIM"))) {
+            int tam = palavra.length() - 1; // índice do último caractere da palavra
+            int i = 0;
+            boolean teste = true; // variável que verifica se é palíndromo
+
+            // compara os caracteres da palavra do início e do fim
+            while(tam > i && teste == true) {
+                if(palavra.charAt(tam) != palavra.charAt(i)) { // se os caracteres forem diferentes
+                    teste = false; 
                 }
                 tam--;
                 i++;
             }
-            if(teste==true){
+
+            // imprime o resultado
+            if(teste == true) {
                 MyIO.println("SIM");
-            }else{
+            } else {
                 MyIO.println("NAO");
             }
-            
+
+            palavra = sc.nextLine(); // lê a próxima palavra para continuar o loop
         }
-        sc.close();
+
+        sc.close(); // fecha o Scanner
     }
 }
