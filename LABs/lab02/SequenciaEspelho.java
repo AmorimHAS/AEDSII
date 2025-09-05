c708d5deb123c32ce7d4563f28c6476fab0dcad0
AmorimHAS <amorim.has@gmail.com>
2025-09-05 15:56:02 -0300

    LAB02-SequeciaEspelho

--- LABs/lab02/SequenciaEspelho.java
+++ LABs/lab02/SequenciaEspelho.java
@@ -8,19 +8,27 @@ public class SequenciaEspelho{
             int fim = sc.nextInt();
             impimirSequencia(inicio,fim);
             imprimirEspelho(inicio,fim);
-            MyIO.println("\n");
+            System.out.println();
         }
+        sc.close();
     }
+
     public static void impimirSequencia(int inicio,int fim){
         for(int i=inicio;i<=fim;i++){
             MyIO.print(i);
         }
     }
+
     public static void imprimirEspelho(int inicio,int fim){
-        int n;
         for(int i=fim;i>=inicio;i--){
-            n=
-            MyIO.print(n);
+            int aux = i;
+            int n;
+            while(aux>=10){
+                n = aux%10;
+                MyIO.print(n);
+                aux = aux/10;
+            }
+            MyIO.print(aux);
         }
     }
     
