e319e28b4e0372988fa939acb212d164c96ba0e2
AmorimHAS <amorim.has@gmail.com>
2025-09-15 15:39:26 -0300

    TP03Q03

---
+++ TPs/TP3/Java/InversaoString.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class InversaoString{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        String frase = sc.nextLine();
+        while(!(frase.equals("FIM"))){
+            int tam = frase.length();
+            Inversao(frase, tam);
+            System.out.println();
+            frase = sc.nextLine();
+        }
+    }
+
+    public static void Inversao (String frase, int tam){
+        if(tam==0){
+            return;
+        }else{
+            System.out.print(frase.charAt(tam-1));
+            tam -=1;
+            Inversao(frase,tam);
+        }
+    }
+}
